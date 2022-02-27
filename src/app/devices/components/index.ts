354c1d80a7bea811c5abd8af17bff91da9244209
adedayojs <samfeolu@gmail.com>
2022-02-28 00:44:57 +0100

    chore: restructure app to reusable component

--- src/app/devices/components/index.ts
+++ src/app/devices/components/index.ts
@@ -1,3 +1,4 @@
+import { DeviceComponent } from '../device.component';
 import { DeviceDetailComponent } from './device-detail/device-detail.component';
 import { DeviceListItemComponent } from './device-list-item/device-list-item.component';
 import { DevicesHomeComponent } from './devices-home/devices-home.component';
@@ -6,10 +7,12 @@ export const components = [
   DevicesHomeComponent,
   DeviceDetailComponent,
   DeviceListItemComponent,
+  DeviceComponent,
 ];
 export * from './device-detail/device-detail.component';
 export * from './devices-home/devices-home.component';
 export * from './devices-home/devices-home.component';
+export * from '../device.component';
 
 // This index File is created and structure in this way because we are assuming that the components in this module will grow larger. We are also assuming that
 // Some developers might not be using the angular cli tool to create new components and might be doing so manually, Therefore it is not automatically added to the module file.
--- src/app/devices/devices-routing.module.ts
+++ src/app/devices/devices-routing.module.ts
@@ -4,12 +4,19 @@ import { NotFoundComponent } from '../shared/components/not-found/not-found.comp
 import {
   DeviceDetailComponent,
   DevicesHomeComponent,
+  DeviceComponent,
 } from './components/index'; // Since all our components are being tracked in the index.ts file we are importing it from here and not from the component file itself
 
 const routes: Routes = [
-  { path: '', component: DevicesHomeComponent },
-  { path: ':id', component: DeviceDetailComponent },
-  { path: '**', component: NotFoundComponent },
+  {
+    path: '',
+    component: DeviceComponent,
+    children: [
+      { path: ':id', component: DeviceDetailComponent },
+      { path: '', component: DevicesHomeComponent },
+      { path: '**', component: NotFoundComponent },
+    ],
+  },
 ];
 
 @NgModule({
