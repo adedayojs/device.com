96e00b626385e8f4ef803d9a9e5bb147188a5a95
adedayojs <samfeolu@gmail.com>
2022-02-27 22:46:49 +0100

    wip: create device seaerch component

--- src/app/devices/devices.module.ts
+++ src/app/devices/devices.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DevicesRoutingModule } from './devices-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 import * as fromComponents from './components';
 
 @NgModule({
   declarations: [...fromComponents.components],
-  imports: [CommonModule, DevicesRoutingModule],
+  imports: [CommonModule, DevicesRoutingModule, SharedModule],
 })
 export class DevicesModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { SearchComponent } from './components/search/search.component';
-
-
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [
-    NotFoundComponent,
-    SearchComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [NotFoundComponent, SearchComponent],
+  exports: [NotFoundComponent, SearchComponent],
+  imports: [CommonModule, MatIconModule],
 })
-export class SharedModule { }
+export class SharedModule {}
